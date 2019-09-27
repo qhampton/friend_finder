1a7316515db50c4c70458e4fdb00fbfdf7e3d327
Quinlan <qnhampton93@gmail.com>
2019-09-26 21:50:45 -0700

    fixed 500 error by defining index in routes, added matching logic in post of routes

--- app/routes/apiRoutes.js
+++ app/routes/apiRoutes.js
@@ -3,7 +3,10 @@
 // These data sources hold arrays of information on friends
 // ===============================================================================
 var friends = require('../data/friends.js');
-
+var findAVG = function (arr){
+    let sum = arr.reduce((previous, current) => current += previous);
+    return sum / arr.length;
+}
 // ROUTING
 // ===============================================================================
 module.exports = function(app){
@@ -16,12 +19,26 @@ module.exports = function(app){
     // Below code handles when a user submits a form and thus submits data to the server.
     app.post('/api/friends',function(req,res){
         var newFriend = req.body;
+        var bestIndex = 0;
+        var bestDiff=999;
+        console.log(req.body);
         //go through the user's data via loop
+        for (var i=0; i < friends.length; i++){
+            var friendAVG = findAVG(friends[i].scores);
+            var newFriendAVG = findAVG(newFriend.scores);
+            console.log ("current friend: " + friendAVG);
+            if(bestDiff > Math.abs(friendAVG-newFriendAVG)){
+                bestDiff = Math.abs(friendAVG-newFriendAVG);
+                bestIndex = i;
+            }            
+        }
+        console.log ("new friend: " + findAVG(newFriend.scores));
         //find the best match by comparing again each friend in db
-        //user another loop to comare the values
+        //user another loop to compare the values
         //calculate the difference in values
         //push new friend into friends array
         friends.push(newFriend);
-        res.json(friends[index]);
+        res.json(friends[bestIndex]);
+
     });
 }
